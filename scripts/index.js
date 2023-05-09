5e908d41b3f97ebefc5e5432773aa16ed5a3628e
milena <katyawey2002@mail.ru>
2023-05-09 17:07:38 +0300

    js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+const buttonsClear = document.getElementById('buttonsClear');
+
 buttonsSend.onclick = function (event) {
 event.preventDefault();
 
@@ -6,8 +8,16 @@ let user = {
     floor: document.getElementById('floor').value,
     room: document.getElementById('room').value,
     data: document.getElementById('data').value,
-    comments: document.getElementById('comments').innerText,
+    comments: document.getElementById('comments').value,
 }
 let userJson = JSON.stringify(user);
 console.log(userJson);
 }
+
+const textarea = document.querySelector('container-comment-text');
+const button = document.querySelector('buttonsClear');
+
+buttonsClear.addEventListener('click', () => {
+    const formData = new FormData();
+    form.formData = formData;
+  });

63d69be204a286b2a802fde3a1dffaac02ce5b8d
milena <katyawey2002@mail.ru>
2023-05-09 15:44:31 +0300

    js2

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,3 @@
-// const form = document.getElementById('container-forma');
-// const tower = form.querySelector('[name="tower"]'),
-// floor = form.querySelector('[name="floor"]'), 
-// room = form.querySelector('[name="room"]'), 
-// data = form.querySelector('[name="data"]'),
-// comments = form.querySelector('[name="comments"]'); 
-
-// form.addEventListener('submit', getFormValue);
-
-// function getFormValue(event) {
-//     event.preventDefault();
-// }
-// const object = {
-//     tower: tower.value,
-//     floor: floor.value,
-//     room: room.value,
-//     data: data.checked,
-//     comments: comments.value
-// };
-// console.log(object);
-
-// const form = document.getElementById('container-forma');
-// form.addEventListener('submit', handleFormSubmit)
-
-// function handleFormSubmit(event) {
-//     // Просим форму не отправлять данные самостоятельно
-//     event.preventDefault()
-//     console.log('Отправка!')
-//   }
-
 buttonsSend.onclick = function (event) {
 event.preventDefault();
 
